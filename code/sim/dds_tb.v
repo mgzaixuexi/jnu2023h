091dc5b22a6acddf4c477fd736f5444252d75e03
mrlin555 <147978648+mrlin555@users.noreply.github.com>
2025-06-04 19:20:29 +0800

    dds的仿真文件
    单独用来仿真dds模块的 可忽略

---
+++ code/sim/dds_tb.v
@@ -0,0 +1,68 @@
+ `timescale 1ns / 1ps //仿真单位/仿真精度
+
+module dds_tb();
+
+parameter CLK_PERIOD = 20; //时钟周期 20ns  50MHz
+//reg define
+ reg sys_clk;
+ reg sys_rst_n;
+ reg clk_100m;
+ //DA芯片接口                        
+ reg                da_clk         ; //DAC驱动时钟
+ wire       [7:0]    in_da_data1         ; //输入波形  
+ wire       [7:0]    in_da_data2         ; //输入波形  
+ reg [5:0] freq_select1; //频率选择寄存器（1表示5kHZ 2表示10kHZ 以此类推）（注意不能为0）
+ reg wave_select1;  //波形选择寄存器（0输出正弦波 1输出三角波）
+ reg phase_select1; //相位寄存器（0表示5° 1表示10°以此类推）
+ reg [5:0] freq_select2; //频率选择寄存器（1表示5kHZ 2表示10kHZ 以此类推）（注意不能为0）
+ reg wave_select2;  //波形选择寄存器（0输出正弦波 1输出三角波）
+ reg phase_select2; //相位寄存器（0表示5° 1表示10°以此类推）
+
+
+ //信号初始化
+ initial begin
+  freq_select1=6'd2;
+  freq_select2=6'd3;
+  wave_select1=1'b0;
+  wave_select2=1'b0;
+    phase_select1=0;
+    phase_select2=0;
+ sys_clk = 1'b0;
+ sys_rst_n = 1'b0;
+ clk_100m=1'b0;
+ #200
+ sys_rst_n = 1'b1;
+#500000
+ phase_select1=1;
+#2000
+ phase_select1=0;
+#500000
+  phase_select1=1;
+#2000
+ phase_select1=0;
+
+ end
+
+ //产生时钟
+ always #(CLK_PERIOD/2) sys_clk = ~sys_clk;
+ always #(CLK_PERIOD/4) clk_100m = ~clk_100m;
+
+
+dds u_dds(
+    .sys_clk(sys_clk),  //系统时钟
+    .sys_rst_n(sys_rst_n),  //系统复位，低电平有效
+    .wave_select1(wave_select1),  //波形控制
+    .wave_select2(wave_select2),  //波形控制
+    .freq_select1(freq_select1),  //频率控制
+    .freq_select2(freq_select2),  //频率控制
+    .phase_select1(phase_select1),   //相位控制
+    .phase_select2(phase_select2),   //相位控制
+    .clk_100m(clk_100m),    //100M时钟
+    //DA芯片接口
+    .da_clk_1(da_clk_1),  //DAC驱动时钟
+    .da_clk_2(da_clk_2),  //DAC驱动时钟
+    .da_data_1(in_da_data1),  //输出给DA的数据
+    .da_data_2(in_da_data2)    //输出给DA的数据
+    );
+endmodule
+
